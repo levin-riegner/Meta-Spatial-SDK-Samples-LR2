c0fe1ace894ac48e8eeee29903c84a4830402629
Gautier de Lataillade <gautier2406@gmail.com>
2025-07-30 11:39:30 +0200

    Implement UISet components in SamplesStateView

--- Showcases/media_view/app/src/main/java/com/meta/levinriegner/mediaview/app/gallery/samples/SamplesStateView.kt
+++ Showcases/media_view/app/src/main/java/com/meta/levinriegner/mediaview/app/gallery/samples/SamplesStateView.kt
@@ -4,22 +4,22 @@ package com.meta.levinriegner.mediaview.app.gallery.samples
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.meta.levinriegner.mediaview.R
 import com.meta.levinriegner.mediaview.app.shared.theme.AppColor
 import com.meta.levinriegner.mediaview.app.shared.theme.Dimens
 import com.meta.levinriegner.mediaview.data.samples.model.SamplesList
 import com.meta.levinriegner.mediaview.app.shared.view.component.BannerView
+import com.meta.spatial.uiset.button.BorderedButton
+import com.meta.spatial.uiset.button.BorderlessButton
 
 @Composable
 fun SamplesStateView(
@@ -38,24 +38,16 @@ fun SamplesStateView(
         modifier = modifier,
         label = stringResource(R.string.sample_media_no_internet),
         buttons = {
-          OutlinedButton(
-            colors = ButtonDefaults.buttonColors(
-              contentColor = AppColor.White,
-              containerColor = Color.Transparent,
-            ),
+          BorderedButton(
+            label = stringResource(R.string.retry),
             onClick = onRefresh,
-          ) {
-            Text(
-              text = stringResource(R.string.retry),
-              color = AppColor.White,
-            )
-          }
-          TextButton(onClick = { onDismiss() }) {
-            Text(
-              text = stringResource(R.string.dismiss),
-              color = AppColor.White,
-            )
-          }
+            borderColor = AppColor.White,
+          )
+          Spacer(modifier = Modifier.size(Dimens.medium))
+          BorderlessButton(
+            label = stringResource(R.string.dismiss),
+            onClick = { onDismiss() },
+          )
         }
       )
     }
@@ -75,24 +67,16 @@ fun SamplesStateView(
         modifier = modifier,
         label = stringResource(R.string.sample_media_available),
         buttons = {
-          OutlinedButton(
-            colors = ButtonDefaults.buttonColors(
-              contentColor = AppColor.White,
-              containerColor = Color.Transparent,
-            ),
+          BorderedButton(
+            label = stringResource(R.string.download),
             onClick = { onDownload(state.samples) },
-          ) {
-            Text(
-              text = stringResource(R.string.download),
-              color = AppColor.White,
-            )
-          }
-          TextButton(onClick = { onDismiss() }) {
-            Text(
-              text = stringResource(R.string.dismiss),
-              color = AppColor.White,
-            )
-          }
+            borderColor = AppColor.White,
+          )
+          Spacer(modifier = Modifier.size(Dimens.medium))
+          BorderlessButton(
+            label = stringResource(R.string.dismiss),
+            onClick = { onDismiss() },
+          )
         }
       )
     }
@@ -112,24 +96,16 @@ fun SamplesStateView(
         modifier = modifier,
         label = stringResource(R.string.sample_media_error, state.message),
         buttons = {
-          OutlinedButton(
-            colors = ButtonDefaults.buttonColors(
-              contentColor = AppColor.White,
-              containerColor = Color.Transparent,
-            ),
+          BorderedButton(
+            label = stringResource(R.string.retry),
             onClick = onRefresh,
-          ) {
-            Text(
-              text = stringResource(R.string.retry),
-              color = AppColor.White,
-            )
-          }
-          TextButton(onClick = { onDismiss() }) {
-            Text(
-              text = stringResource(R.string.dismiss),
-              color = AppColor.White,
-            )
-          }
+            borderColor = AppColor.White,
+          )
+          Spacer(modifier = Modifier.size(Dimens.medium))
+          BorderlessButton(
+            label = stringResource(R.string.dismiss),
+            onClick = { onDismiss() },
+          )
         }
       )
     }
@@ -138,12 +114,10 @@ fun SamplesStateView(
         modifier = modifier,
         label = stringResource(R.string.sample_media_success),
         buttons = {
-          TextButton(onClick = { onDismiss() }) {
-            Text(
-              text = stringResource(R.string.dismiss),
-              color = AppColor.White,
-            )
-          }
+          BorderlessButton(
+            label = stringResource(R.string.dismiss),
+            onClick = { onDismiss() },
+          )
         }
       )
     }
